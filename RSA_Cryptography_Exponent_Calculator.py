aa99f79f9f33214882bb9b57f07cee973161c29f
Quality9 <ramepassi@gmail.com>
2025-05-23 11:26:21 -0500

    Update and rename RSA_Cryptography.py to RSA_Cryptography_Exponent_Calculator.py
    Can raise a number to a very high power and mod it by some number. Does this using conversion to binary, squaring and modding with each step, and multiplying when matching index in binary form of exponent == 1.

--- RSA_Cryptography_Exponent_Calculator.py
+++ RSA_Cryptography_Exponent_Calculator.py
@@ -1,14 +1,18 @@
 def quick_mod(large_base, large_exponent, mod):
-    
+
+    #Convert Exponent to Binary form- ascending order
     binary_exponent = bin(large_exponent)[2:][::-1]
+    
     multiplier_list = [large_base % mod]
     product = 1
 
+    #Constructing multiplier list using mod, multiplying if matching index in binary string is 1
     for i in range(1, len(binary_exponent)):
         multiplier_list.append((multiplier_list[-1] ** 2) % mod)
         if binary_exponent[i] == '1':
             product *= multiplier_list[-1]
-    
+
+    #Last multiplication
     if binary_exponent[0] == '1':
         product *= large_base
     return product % mod
